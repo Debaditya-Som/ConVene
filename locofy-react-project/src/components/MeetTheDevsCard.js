29bf4886919a227a3da0a27dc40d5c9cbc46dc10
Anushka Bhowmick <76967222+Anushka-Bhowmick@users.noreply.github.com>
2023-08-28 06:15:42 +0530

    initial commit

---
+++ locofy-react-project/src/components/MeetTheDevsCard.js
@@ -0,0 +1,45 @@
+const MeetTheDevsCard = () => {
+  return (
+    <div className="absolute top-[13000px] left-[0px] w-[4695px] h-[4728px] mix-blend-normal text-left text-231xl text-gray-200 font-h1-heading">
+      <h1 className="m-0 absolute top-[0px] left-[1520px] text-inherit [text-decoration:underline] capitalize font-semibold font-inherit">
+        meet the devs
+      </h1>
+      <div className="absolute top-[448px] left-[0px] bg-gray-300 w-[4695px] h-[4280px]" />
+      <div className="absolute top-[calc(50%_-_1544px)] right-[504px] rounded-[50%] bg-lightskyblue [filter:blur(800px)] box-border w-[3734px] h-[3606px] border-[1px] border-solid border-deeppink" />
+      <div className="absolute top-[1998px] left-[349px] capitalize font-semibold text-snow-300 inline-block w-[1218px] h-[261px]">
+        Anushka
+      </div>
+      <img
+        className="absolute top-[570px] left-[3196px] rounded-81xl w-[1224px] h-[1364px] object-cover"
+        alt=""
+        src="/name-block@2x.png"
+      />
+      <img
+        className="absolute top-[2770px] left-[349px] rounded-81xl w-[1188.71px] h-[1364px] object-cover"
+        alt=""
+        src="/image-10@2x.png"
+      />
+      <div className="absolute top-[1960px] left-[3358px] capitalize font-semibold text-snow-200 inline-block w-[874px] h-[152px]">
+        Akarsit
+      </div>
+      <div className="absolute top-[4175px] left-[571px] capitalize font-semibold text-snow-100 inline-block w-[1131px] h-[109px]">
+        Kunal
+      </div>
+      <h1 className="m-0 absolute top-[4132px] left-[3183px] text-inherit capitalize font-semibold font-inherit text-whitesmoke inline-block w-[1385px] h-[152px]">
+        Debaditya
+      </h1>
+      <img
+        className="absolute top-[596px] left-[255px] rounded-81xl w-[1224px] h-[1364px] object-cover"
+        alt=""
+        src="/name-block1@2x.png"
+      />
+      <img
+        className="absolute top-[2783px] left-[3183px] rounded-81xl w-[1224px] h-[1364px] object-cover"
+        alt=""
+        src="/name-block2@2x.png"
+      />
+    </div>
+  );
+};
+
+export default MeetTheDevsCard;
---
+++ locofy-react-project/src/components/RegisterFormContainer.js
@@ -0,0 +1,30 @@
+const RegisterFormContainer = () => {
+  return (
+    <section className="absolute top-[9504px] left-[668px] w-[3367px] h-[2695px] text-left text-45xl text-black font-jacques-francois">
+      <div className="absolute top-[2519px] left-[903px] rounded-131xl bg-skyblue w-[658px] h-44" />
+      <div className="absolute top-[2519px] left-[0px] rounded-131xl bg-white w-[658px] h-44" />
+      <div className="absolute top-[2519px] left-[2709px] rounded-131xl bg-gray-100 w-[658px] h-44" />
+      <div className="absolute top-[2519px] left-[1806px] rounded-131xl bg-skyblue w-[658px] h-44" />
+      <div className="absolute top-[2565px] left-[1065px] uppercase">
+        REgister
+      </div>
+      <img
+        className="absolute top-[384px] left-[0px] w-[3367px] h-[1936px] object-cover"
+        alt=""
+        src="/rectangle-24@2x.png"
+      />
+      <div className="absolute top-[2565px] left-[1978px] uppercase">
+        Details
+      </div>
+      <div className="absolute top-[2565px] left-[2922px] uppercase">Next</div>
+      <div className="absolute top-[2565px] left-[115px] uppercase">
+        previous
+      </div>
+      <div className="absolute top-[0px] left-[197px] text-231xl capitalize font-semibold font-h1-heading text-white">
+        Currently Ongoing Events:
+      </div>
+    </section>
+  );
+};
+
+export default RegisterFormContainer;
